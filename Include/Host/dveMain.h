88943c97d5d61f7078c50fc584e07cfd6fad51e9
RyanLim <limyh14@gmail.com>
2014-12-03 18:44:47 +0900

    Initial Commit

---
+++ Include/Host/dveMain.h
@@ -0,0 +1,4 @@
+#ifndef DVE_MAIN_H_
+#define DVE_MAIN_H_
+
+#endif /* DVE_MAIN_H_ */
---
+++ Source/Host/dveMain.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+//#include "dveMain.h"
+
+int main()
+{
+	printf("This is DVE Main Program!\n");
+}
